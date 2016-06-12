f81a562af354ea05f6f413993a7f3a636d7033a2
janingyi <janingyi@163.com>
2016-06-12 16:06:59 +0800

    Add files via upload

---
+++ StudentManagementSystem/web/mock.js
@@ -0,0 +1,94 @@
+{
+		"fullName":"Shino吴",
+		"weixinName":"ShinoCreation吴",
+		"courseType":"P",//professional
+		"expectEndDate":"",
+		"practicalSessions":[{
+				"practicalname":"番禺Daisy婚礼",
+				"practicaldate":"",
+				"remark":"上午实习，5点到"},
+			{
+				"practicalname":"LOCK婚礼",
+				"practicaldate":"",
+				"remark":"上午实习，5点到"}
+		]
+		"totalCourseAmount":50,
+		"leftCourseAmount":20,
+		"phone":"1234567",
+		"age":"23",
+		"dealAmount":18888,
+		"dealType":"f",//options:分期f，一次性y，微信w，思明s
+		"dealDate":"",
+		"birthday":"",
+		"eMail":"ShinoCreation@qq.com"		
+},
+{
+		"fullName":"Jim",
+		"weixinName":"Ning",
+		"courseType":"J",//Junior, personal
+		"expectEndDate":"",
+		"practicalSessions":[{
+				"practicalname":"Fanny婚礼拍摄",
+				"practicaldate":"",
+			"remark":"全天实习"},
+			{
+				"practicalname":"kimi婚礼拍摄",
+				"practicaldate":"",
+			"remark":"全天实习"}
+		]
+		"totalCourseAmount":30,
+		"leftCourseAmount":10,
+		"phone":"1234567",
+		"age":"23",
+		"dealAmount":8888,
+		"dealType":"y",//一次性，微信，思明
+		"dealDate":"",
+		"birthday":"",
+		"eMail":"ShinoCreation@qq.com"		
+},
+{
+		"fullName":"Shino",
+		"weixinName":"ShinoCreation",
+		"courseType":"J",//Junior, personal
+		"expectEndDate":"",
+		"practicalSessions":[
+				"practicalname":"",
+				"practicaldate":"",
+				"remark":""
+		]
+		"totalCourseAmount":"",
+		"leftCourseAmount":"",
+		"phone":"",
+		"age":"",
+		"amount":"",
+		"dealDate":"",
+		"birthday":"",
+		"eMail":""		
+}
+	
+	 <label for="fullname">全名：</label>
+	        <input type="text" name="fullname" id="fullname">
+	        <label for="weixinname">微信名：</label>
+	        <input type="text" name="weixinname" id="weixinname">    
+	        <label for="coursetype">课程类型：</label>
+	        <input type="text" name="coursetype" id="coursetype" placeholder="专业 or 个人"> 
+	        <label for="phone">手机</label>
+	        <input type="text" name="phone" id="phone"> 
+	        <label for="age">年龄：</label>
+	        <input type="text" name="age" id="age"> 
+	        <label for="weixinname">价钱：</label>
+	        <input type="text" name="weixinname" id="weixinname"> 
+	        <label for="weixinname">总课时：</label>
+	        <input type="text" name="weixinname" id="weixinname"> 
+	        <label for="weixinname">剩余课时：</label>
+	        <input type="text" name="weixinname" id="weixinname"> 
+	        <label for="weixinname">缴费日期：</label>
+	        <input type="text" name="weixinname" id="weixinname">    
+	        <label for="bday">生日：</label>
+	        <input type="date" name="bday" id="bday">
+	        <label for="email">E-mail:</label>
+	        <input type="email" name="email" id="email" placeholder="你的电子邮箱..">
+	        <label for="email">期望结课日期:</label>
+	        <input type="email" name="email" id="email" placeholder="你的电子邮箱..">
+	        <label for="weixinname">实习情况：？？？another page？ phase2</label>
+	        <input type="text" name="weixinname" id="weixinname">   
